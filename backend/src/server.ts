bc07b34200efe82d333ae513bd8c8f711ee031c2
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-10-28 14:18:44 -0300

    mend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
 
 app.get('/get', (req, res) => {
-    return res.json('OLÁ MUNDO!')
+    return res.json('Hello world!')
 })
 
 app.listen({ port: 3333 }, () => { console.log('Server is running!') })
