ab1afa27dafc2f395012ff630c3c7b0bb1eb2a88
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-10-28 18:23:00 -0300

    [backend]|chore: Add prisma and DB

---
+++ backend/prisma/migrations/20231028183236_teste/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "phone" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
