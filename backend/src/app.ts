70795aa02f65828f21eb01af1928c16230bdf0fb
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-10-30 00:56:17 -0300

    [backend]| chore: Add body-parser lib and new scripts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import { phoneRouter } from './routes/phoneRoute'
+import bodyParser from 'body-parser'
 
 export const app = express()
 
+app.use(bodyParser.json())
 app.use(cors())
 app.use(morgan('tiny'))
 app.use('/phone/', phoneRouter)
